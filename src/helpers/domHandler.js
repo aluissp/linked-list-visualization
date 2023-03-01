4139f2374a5abd760f813dabaae053e54298c5b2
Luis Perugachi <luisprg05@gmail.com>
2023-03-01 16:27:33 -0500

    Move insert node and pointer to animations.js

--- src/helpers/domHandler.js
+++ src/helpers/domHandler.js
@@ -1,8 +1,7 @@
 import {
 	createNodeAnimation,
 	createPointerAnimation,
-	removeMoveLeftNodeAndPointer,
-	setMoveLeftNodeAndPointer,
+	insertNodeAndPointerAnimation,
 } from '../tools/animations.js';
 import { getHtmlWrappers } from './getHtmlWrappers.js';
 
@@ -22,39 +21,10 @@ export const setNodeInDocument = (htmlNode, pointer) => {
 
 export const insertNodeInDocument = newNode => {
 	return new Promise(async (resolve, reject) => {
-		let nextNode = newNode.next;
 		const [listWrapper] = getHtmlWrappers();
 
-		while (nextNode) {
-			const htmlNode = nextNode.getHtml();
-			const htmlPointer = nextNode.getPointer();
-			setMoveLeftNodeAndPointer({ htmlNode, htmlPointer });
+		await insertNodeAndPointerAnimation({ newNode, listWrapper });
 
-			nextNode = nextNode.next;
-		}
-
-		setTimeout(async () => {
-			let nextNode = newNode.next;
-
-			const afterHtmlNode = nextNode.getHtml();
-			const node = newNode.getHtml();
-			const pointer = newNode.getPointer();
-
-			listWrapper.insertBefore(node, afterHtmlNode);
-			listWrapper.insertBefore(pointer, afterHtmlNode);
-			pointer.firstChild.style.display = 'none';
-			await createNodeAnimation(node);
-			pointer.firstChild.style.display = 'block';
-			await createPointerAnimation(pointer);
-
-			while (nextNode) {
-				const htmlNode = nextNode.getHtml();
-				const htmlPointer = nextNode.getPointer();
-				removeMoveLeftNodeAndPointer({ htmlNode, htmlPointer });
-
-				nextNode = nextNode.next;
-			}
-			resolve();
-		}, 1000);
+		resolve();
 	});
 };
--- src/tools/animations.js
+++ src/tools/animations.js
@@ -41,15 +41,52 @@ export const searchingPointerAnimation = (htmlPointer, time = 800) => {
 	});
 };
 
-export const setMoveLeftNodeAndPointer = ({ htmlNode, htmlPointer }, time = 800) => {
-	// return
+const setMoveLeftNodeAndPointer = ({ htmlNode, htmlPointer }, time = 800) => {
 	htmlNode.style.animation = `moveRightNode ${convertToSeconds(time)}s ease-out`;
 	htmlPointer.style.animation = `moveRightNode ${convertToSeconds(time)}s ease-out`;
 };
 
-export const removeMoveLeftNodeAndPointer = ({ htmlNode, htmlPointer }) => {
+const removeMoveLeftNodeAndPointer = ({ htmlNode, htmlPointer }) => {
 	htmlNode.style.animation = null;
 	htmlPointer.style.animation = null;
+};
+
+export const insertNodeAndPointerAnimation = ({ newNode, listWrapper }, time = 800) => {
+	return new Promise((resolve, reject) => {
+		let nextNode = newNode.next;
+
+		while (nextNode) {
+			const htmlNode = nextNode.getHtml();
+			const htmlPointer = nextNode.getPointer();
+			setMoveLeftNodeAndPointer({ htmlNode, htmlPointer }, time);
+
+			nextNode = nextNode.next;
+		}
+
+		setTimeout(async () => {
+			let nextNode = newNode.next;
+
+			const afterHtmlNode = nextNode.getHtml();
+			const node = newNode.getHtml();
+			const pointer = newNode.getPointer();
+
+			listWrapper.insertBefore(node, afterHtmlNode);
+			listWrapper.insertBefore(pointer, afterHtmlNode);
+			pointer.firstChild.style.display = 'none';
+			await createNodeAnimation(node);
+			pointer.firstChild.style.display = 'block';
+			await createPointerAnimation(pointer);
+
+			while (nextNode) {
+				const htmlNode = nextNode.getHtml();
+				const htmlPointer = nextNode.getPointer();
+				removeMoveLeftNodeAndPointer({ htmlNode, htmlPointer });
+
+				nextNode = nextNode.next;
+			}
+			resolve();
+		}, time);
+	});
 };
 
 const convertToSeconds = milliseconds => milliseconds / 1000;
